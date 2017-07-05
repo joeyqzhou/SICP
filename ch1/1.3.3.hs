7d31c9262cc90baa17449af5a626ab70bb37ab31
joeyqzhou(周琦) <joeyqzhou@tencent.com>
2017-07-05 09:37:12 +0800

    learn trace

--- ch1/1.3.3.hs
+++ ch1/1.3.3.hs
@@ -1,10 +1,19 @@
-fixed_point :: (Floating a, Ord a) => (a->a) -> a ->a
+import Text.Printf
+import Debug.Trace
+
+
+--use Debug.Trace.trace for debuging.
+--trace :: String -> a ->a
+--trace a b; print a, return b
+fixed_point :: (Floating a, Ord a,Show a ) => (a->a) -> a ->a
 fixed_point f guess = try guess 
     where 
     --try :: (Num a) => a -> a
-    try guess = if abs (guess - f guess) < 1.0e-4 
-                    then guess
-                    else try  (f guess)
+    try guess = trace (show guess)  (if abs (guess - f guess) < 1.0e-4 
+                    then do guess
+                    else do
+                        try  (f guess)
+										)
 
 
 search :: (Floating a,Ord a) => (a->a) -> a -> a -> a
@@ -20,4 +29,7 @@ half_interval_method f x y
      | (f x) < 0 && (f y)>0 = search f y x
      | otherwise = 0.0
 
-      
+    
+
+--1.35
+--fixed_point (\x -> (1 + 1.0/x)) 0.0 
